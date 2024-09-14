2f8ee0b496b94c318a1ca5fda6838632851d3bbe
Grigoriy <grishayugov@mail.ru>
2024-09-15 01:45:37 +0300

    rev

--- src/main/java/com/example/action/files/ActionFiles.java
+++ src/main/java/com/example/action/files/ActionFiles.java
@@ -2,7 +2,6 @@ package com.example.action.files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -10,44 +9,31 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+
 
 @Service
 public class ActionFiles {
-
-    private final Deque<Path> fileList = new ArrayDeque<>();
     private static final Logger log = LoggerFactory.getLogger(ActionFiles.class);
-    @Value(value = "${directory.path.scanned}")
-    private String scannedDirectoryPath;
-    @Value(value = "${directory.path.target}")
-    private String targetPath;
-    @Value(value = "${directory.path.duplicate}")
-    private String duplicateFile;
 
+    public List<Path> scanDirectory(String scannedDirectoryPath) {
+        List<Path> fileList = new ArrayList<>();
 
-    public void doJob() {
-        System.out.println("1");
-        //просканировать дир, получить список файлов
-        //пройтись по списку файлов , перенести файлы
-        //пройтись по новой директории, распарсить каждый файл
-        // перенести все файлы в завершено
-        //move
-    }
-
-
-    public void scanDirectory() {
         try (Stream<Path> paths = Files.walk(Paths.get(scannedDirectoryPath))) {
-            paths.filter(Files::isRegularFile)
-                    .forEach(file -> {
-                        log.info("Found file: {}", file.getFileName());
-                        fileList.addLast(file);
-                    });
+            fileList = paths.filter(Files::isRegularFile)
+                    .peek(file -> log.info("Found file: {}", file.getFileName()))
+                    .collect(Collectors.toList());
         } catch (IOException e) {
             log.error("Error scanning directory", e);
         }
+
+        return fileList;
     }
 
-    public void moveFile(Path file) {
+
+    public void moveFile(Path file,String targetPath) {
         Path targetDirectory = Paths.get(targetPath);
         Path targetPathMove = targetDirectory.resolve(file.getFileName());
         try {
@@ -58,24 +44,5 @@ public class ActionFiles {
         }
     }
 
-    public boolean checkForDuplicatesManually() {
-        Set<Path> seen = new HashSet<>();
-        for (Path element : fileList) {
-            if (!seen.add(element)) {
-                return true;
-            }
-        }
-        return false;
-    }
 
-    public Path getLastFile() {
-        return fileList.pollFirst();
-    }
-    public boolean fileListIsEmpty(){
-        return fileList.isEmpty();
-    }
-
-    public Deque<Path> getFileList() {
-        return fileList;
-    }
 }
--- src/main/java/com/example/server/ServerStarter.java
+++ src/main/java/com/example/server/ServerStarter.java
@@ -2,13 +2,24 @@ package com.example.server;
 
 import com.example.action.files.ActionFiles;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Optional;
+
 @EnableScheduling
 @Component
 public class ServerStarter {
+    @Value(value = "${directory.path.scanned}")
+    private String scannedDirectoryPath;
+    @Value(value = "${directory.path.target}")
+    private String targetPath;
+    @Value(value = "${directory.path.duplicate}")
+    private String duplicateFile;
     ActionFiles actionFiles;
 
     @Autowired
@@ -18,13 +29,10 @@ public class ServerStarter {
 
 
     @Scheduled(fixedRateString = "${scan.millis}")
-    public void startServer() {
-        actionFiles.scanDirectory();
-
-        while (!actionFiles.fileListIsEmpty()) {
-            if(actionFiles.checkForDuplicatesManually()) {
-                actionFiles.moveFile(actionFiles.getLastFile());
-            }
+    public void workServer() {
+        List<Path>fileList=actionFiles.scanDirectory(scannedDirectoryPath);
+        for (Path path : fileList) {
+            actionFiles.moveFile(path, targetPath);
         }
     }
 }
