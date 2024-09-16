d6da89d2483cd22e8f1227015fb9d7bdcaf7cee4
Grigoriy <grishayugov@mail.ru>
2024-09-16 13:37:31 +0300

    dev

--- src/main/java/com/example/repo/FileDeviationDtoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.repo;
-
-import com.example.file.dto.PaymentEntryDeviationDto;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FileDeviationDtoRepository extends JpaRepository<PaymentEntryDeviationDto, Long> {
-}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+CREATE TABLE payment_table_error (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    payment VARCHAR(255)
+);
